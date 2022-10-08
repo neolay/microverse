d3d126722812abae4b03e6fe3be125b732e649eb
neolay <26569715+neolay@users.noreply.github.com>
2022-10-08 11:58:26 +0800

    render pixel

--- behaviors/diverse/pixel.js
+++ behaviors/diverse/pixel.js
@@ -1,5 +1,6 @@
 class PixelActor {
     setup() {
+        this.leds = [];
         const pixelX = this._cardData.pixelX || 5;
         const pixelY = this._cardData.pixelY || 5;
         const spacingCol = this._cardData.spacingCol || 0.05;
@@ -10,21 +11,63 @@ class PixelActor {
         const boardHeight = this._cardData.height ||= ledHeight * pixelY + spacingRow * (pixelY + 1);
         const boardDepth = this._cardData.depth ||= 0.05;
 
-        for (let i = 0; i < pixelX; i++) {
-            for (let j = 0; j < pixelY; j++) {
-                this.createCard({
-                    translation: [(2 * i + 1) / 2 * ledWidth + spacingCol * (i + 1) - boardWidth / 2,
-                        -(2 * j + 1) / 2 * ledHeight - spacingRow * (j + 1) + boardHeight / 2, 0],
-                    name: `led-${i}-${j}`,
+        for (let x = 0; x < pixelX; x++) {
+            for (let y = 0; y < pixelY; y++) {
+                const led = this.createCard({
+                    translation: [(2 * x + 1) / 2 * ledWidth + spacingCol * (x + 1) - boardWidth / 2,
+                        -(2 * y + 1) / 2 * ledHeight - spacingRow * (y + 1) + boardHeight / 2, 0],
+                    name: `led-${x}-${y}`,
                     behaviorModules: ["LED"],
                     parent: this,
                     type: "object",
                     width: ledWidth,
                     height: ledHeight,
-                    coordinate: [i, j],
+                    coordinate: [x, y],
                 });
+                this.leds.push(led);
             }
         }
+
+        this.state = this.initialState();
+    }
+
+    initialState() {
+        return [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ];
+    }
+
+    setPixel(x, y, color) {
+        this.state[x][y] = color;
+        this.render();
+        console.log("setPixel", this.state);
+    }
+
+    render() {
+        for (let x = 0; x < 5; x++) {
+            for (let y = 0; y < 5; y++) {
+                const on = this.state[x][y];
+                const led = this.leds[x * 5 + y];
+                if (on) {
+                    const color = this.state[x][y];
+                    this.publish(led.id, "ledOn", color);
+                } else {
+                    this.publish(led.id, "ledOff");
+                }
+            }
+        }
+    }
+
+    getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    getRandomColor() {
+        return new Microverse.THREE.Color(Math.random(), Math.random(), Math.random());
     }
 }
 
@@ -56,11 +99,28 @@ class LEDPawn {
         const width = this.actor._cardData.width;
         const height = this.actor._cardData.height;
 
-        const led = new THREE.Mesh(
+        this.led = new THREE.Mesh(
             new THREE.BoxGeometry(width, height, 0.1, 2, 2, 2),
-            new THREE.MeshStandardMaterial({color: 0xEEEEEE, emissive: 0xEEEEEE}));
+            new THREE.MeshBasicMaterial({color: 0x000000}));
+        this.light = new THREE.PointLight(0x000000, 0, 3);
+        this.led.add(this.light);
 
-        this.shape.add(led);
+        this.shape.add(this.led);
+
+        this.subscribe(this.actor.id, "ledOn", "ledOn");
+        this.subscribe(this.actor.id, "ledOff", "ledOff");
+    }
+
+    ledOn(color) {
+        console.log("subscribe ledOn", this.actor.id, this.led, color);
+        this.led.material.color.set(color);
+        this.light.color.set(color);
+        this.light.intensity = 1;
+    }
+
+    ledOff() {
+        this.led.material.color.set(0x000000);
+        this.light.intensity = 0;
     }
 }
 
@@ -78,3 +138,5 @@ export default {
         }
     ]
 }
+
+/* globals Microverse */
