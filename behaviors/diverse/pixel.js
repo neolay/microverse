41e7f36918ef2748aa70427f3e07f724e4cd3afe
neolay <26569715+neolay@users.noreply.github.com>
2022-10-09 18:49:51 +0800

    show rainbow

--- behaviors/diverse/pixel.js
+++ behaviors/diverse/pixel.js
@@ -356,7 +356,9 @@ class StripDisplayActor {
         this.pixelY = this._cardData.pixelY || 1;
         this.state = this.initialState(this.pixelX, this.pixelY);
 
-        this.random();
+        // this.random();
+        this.showRainbow();
+        this.wave();
     }
 
     initialState(x, y) {
@@ -372,6 +374,25 @@ class StripDisplayActor {
         return new Microverse.THREE.Color(Math.random(), Math.random(), Math.random());
     }
 
+    rotate(offset = 1) {
+        const gcd = (x, y) => y ? gcd(y, x % y) : x;
+        const arr = this.state;
+        const n = arr.length;
+        offset = offset % n;
+        let count = gcd(offset, n);
+        for (let start = 0; start < count; ++start) {
+            let current = start;
+            let prev = arr[start];
+            do {
+                const next = (current + offset) % n;
+                const temp = arr[next];
+                arr[next] = prev;
+                prev = temp;
+                current = next;
+            } while (start !== current);
+        }
+    }
+
     random() {
         for (let x = 0; x < this.pixelX; x++) {
             for (let y = 0; y < this.pixelY; y++) {
@@ -379,9 +400,115 @@ class StripDisplayActor {
             }
         }
         this.say("render");
-        this.future(50).random();
+        this.future(100).random();
     }
 
+    wave() {
+        this.rotate(10);
+        this.say("render");
+        this.future(100).wave();
+    }
+
+    showRainbow(startHue = 1, endHue = 360) {
+        const idiv = (x, y) => {
+            return ((x | 0) / (y | 0)) | 0
+        }
+
+        startHue = startHue >> 0;
+        endHue = endHue >> 0;
+        const saturation = 100;
+        const luminance = 50;
+        const steps = this.pixelX;
+
+        const h1 = startHue;
+        const h2 = endHue;
+        const hDistCW = ((h2 + 360) - h1) % 360;
+        const hStep = idiv((hDistCW * 100), steps);
+
+        const h1_100 = h1 * 100;
+
+        const s1 = saturation;
+        const sDist = saturation - s1;
+        const sStep = idiv(sDist, steps);
+        const s1_100 = s1 * 100;
+
+        const l1 = luminance;
+        const lDist = luminance - l1;
+        const lStep = idiv(lDist, steps);
+        const l1_100 = l1 * 100
+
+        if (steps === 1) {
+            this.setPixel(0, 0, this.hsl(h1 + hStep, s1 + sStep, l1 + lStep));
+        } else {
+            this.setPixel(0, 0, this.hsl(startHue, saturation, luminance));
+            for (let i = 1; i < steps - 1; i++) {
+                const h = idiv((h1_100 + i * hStep), 100) + 360;
+                const s = idiv((s1_100 + i * sStep), 100);
+                const l = idiv((l1_100 + i * lStep), 100);
+                this.setPixel(i, 0, this.hsl(h, s, l));
+            }
+            this.setPixel(steps - 1, 0, this.hsl(endHue, saturation, luminance));
+        }
+
+        this.say("render");
+    }
+
+    hsl(h, s, l) {
+        const idiv = (x, y) => {
+            return ((x | 0) / (y | 0)) | 0
+        }
+
+        const clamp = (min, max, value) => {
+            return Math.min(max, Math.max(min, value));
+        }
+
+        h = Math.round(h);
+        s = Math.round(s);
+        l = Math.round(l);
+
+        h = h % 360;
+        s = clamp(0, 99, s);
+        l = clamp(0, 99, l);
+        const c = idiv((((100 - Math.abs(2 * l - 100)) * s) << 8), 10000);
+        const h1 = idiv(h, 60);
+        const h2 = idiv((h - h1 * 60) * 256, 60);
+        const temp = Math.abs((((h1 % 2) << 8) + h2) - 256);
+        const x = (c * (256 - (temp))) >> 8;
+        let r$;
+        let g$;
+        let b$;
+        if (h1 === 0) {
+            r$ = c;
+            g$ = x;
+            b$ = 0;
+        } else if (h1 === 1) {
+            r$ = x;
+            g$ = c;
+            b$ = 0;
+        } else if (h1 === 2) {
+            r$ = 0;
+            g$ = c;
+            b$ = x;
+        } else if (h1 === 3) {
+            r$ = 0;
+            g$ = x;
+            b$ = c;
+        } else if (h1 === 4) {
+            r$ = x;
+            g$ = 0;
+            b$ = c;
+        } else if (h1 === 5) {
+            r$ = c;
+            g$ = 0;
+            b$ = x;
+        }
+        const m = idiv((idiv((l * 2 << 8), 100) - c), 2);
+        const r = r$ + m;
+        const g = g$ + m;
+        const b = b$ + m;
+
+        return new Microverse.THREE.Color(`rgb(${r}, ${g}, ${b})`);
+    }
 }
 
 class StripDisplayPawn {
